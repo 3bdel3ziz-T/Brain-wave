01cbf9c98f1777944d15f4b026be9f7ea9f2b937
3bdel3zizT <zizotamercomz@gmail.com>
2025-03-09 14:08:03 +0200

    adding the constant file

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import "./App.css";
-import { Routes, Route as Link, Link as ToLink } from "react-router-dom";
+import Header from "./components/Header";
 // import { Outlet } from "react-router";
 
-function App() {
+const App = () => {
 	return (
 		<>
-			<h1 className="underline text-3xl ">Hi there</h1>
-			<Routes>
-				<Link
-					path="/"
-					element={<ToLink to="/dashboard">to the dashboard</ToLink>}
-				>
-					{/* renders into the outlet in <Root> at "/" */}
-					{/* <Link index element={<h1>Home</h1>} /> */}
-
-					<Link path="dashboard" element={<h1>Dashboard</h1>}>
-						{/* renders into the outlet in <Dashboard> at "/dashboard" */}
-						<Link index element={<h1>Dashboard Home</h1>} />
-						{/* <Link path="settings" element={<h1>settings</h1>} /> */}
-					</Link>
-				</Link>
-			</Routes>
+			<Header/>
 		</>
 	);
-}
+};
 
 export default App;
